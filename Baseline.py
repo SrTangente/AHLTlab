1d1d2be1866213a6be5e2ae80586874c0fcb8e62
SrTangente <josejuangalan@gmail.com>
2021-02-22 21:03:31 +0100

    Solved encoding problems

--- Baseline.py
+++ Baseline.py
@@ -3,7 +3,7 @@ import nltk
 from xml.dom.minidom import parse, parseString
 from nltk.tokenize import word_tokenize
 import evaluator
-
+from nltk.corpus import stopwords
 
 DATADIR = './data/train/'
 OUTFILENAME = './results.txt'
@@ -11,13 +11,31 @@ OUTFILENAME = './results.txt'
 
 def tokenize(text):
     words = word_tokenize(text)
-    return [(word, text.index(word), text.index(word)+len(word)) for word in words]
+    print(words)
+    result = []
+    for word in words:
+        try:
+            result.append((word, text.index(word), text.index(word)+len(word)))
+        except:
+            print("Some word could not be readed")
+    return result
 
+def get_resources():
+    db = open('./resources/DrugBank.txt', 'r', encoding="latin1").read()
+    drugBank = set(word_tokenize(db))
+    punctuation = [',', '.', ':', ';', '?', '!', '"', "'"]
+    sw = set(stopwords.words('english'))
+    for w in sw:
+        if w in drugBank:
+            drugBank.remove(w)
+    for w in punctuation:
+        if w in drugBank:
+            drugBank.remove(w)
+    return [drugBank]
 
 # Process each file in directory
-def extract_entities(tokens):
-    db = open('./resources/DrugBank.txt', 'r').read()
-    drugBank = word_tokenize(db)
+def extract_entities(tokens, resources):
+    drugBank = resources[0]
     entities = []
     for t in tokens:
         word = t[0]
@@ -36,10 +54,13 @@ def extract_entities(tokens):
             entities.append(entity)
     return entities
 
-
+line = 0
 OUTFILE = open(OUTFILENAME, 'w+')
 
+resources = get_resources()
 for f in listdir(DATADIR):
+    line = line + 1
+    print(line)
     # parse XML file , obtaining a DOM tree
     tree = parse(DATADIR + "/" + f)
     # process each sentence in the file
@@ -50,10 +71,11 @@ for f in listdir(DATADIR):
         # tokenize text
         tokens = tokenize(stext)
         # extract entities from tokenized sentence text
-        entities = extract_entities(tokens)
+        entities = extract_entities(tokens, resources)
         # print sentence entities in format requested for evaluation
         for e in entities:
             print(sid +"|" + e["offset"] +"|" + e["text"] + "|" + e["type"], file=OUTFILE)
 
 # print performance score
-evaluator.evaluate("NER", DATADIR, OUTFILE)# process each file in directory
+evaluator.evaluate("NER", DATADIR, OUTFILE)
+# process each file in directory
