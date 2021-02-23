9a866a7785d2b0d71c52a67ffe7dd8dc5d086ce4
SrTangente <josejuangalan@gmail.com>
2021-02-23 19:15:12 +0100

    Update Baseline.py

--- Baseline.py
+++ Baseline.py
@@ -5,7 +5,7 @@ from nltk.tokenize import word_tokenize
 import evaluator
 
 
-DATADIR = './data/train/'
+DATADIR = './data/devel/'
 OUTFILENAME = './results.txt'
 
 
@@ -15,12 +15,15 @@ def load_drug_bank():
     :return: the dictionary is in the form of {"drug_name": "drug|brand|group"}
     """
     drug_bank = {}
-    with open('./resources/DrugBank.txt', 'r') as db:
+    with open('./resources/DrugBank.txt', 'r', encoding='utf8') as db:
         line = db.readline()
         while line:
-            [name, type] = line.split('|')
-            drug_bank[name] = type
-            line = db.readline()
+            try:
+                [name, type] = line.split('|')
+                drug_bank[name.lower()] = type
+                line = db.readline()
+            except UnicodeDecodeError:
+                print('Omitting line')
 
     return drug_bank
 
@@ -34,25 +37,65 @@ def tokenize(text):
     return [(word, text.index(word), text.index(word)+len(word)-1) for word in words if "&" not in word]
 
 
+
+def check_suffixes(words):
+    prefixes = ['cef', 'ceph', 'cort', 'pred', 'sulfa']
+    suffixes = ['azole', 'idine', 'amine', 'mycin', 'asone', 'bicin', 'afil', 'bital',
+                'caine', 'cillin', 'cycline', 'dipine', 'dronate', 'eprazole', 'fenac',
+                'floxacin', 'gliptin', 'glitazone', 'iramine', 'mab', 'lamide', 'mustine',
+                'mycin', 'nacin', 'olol', 'odone', 'olone', 'onide', 'parin', 'phylline', 'pril',
+                'profen', 'ridone', 'sartan', 'semide', 'setron', 'statin', 'terol',
+                'thiazide', 'tinib', 'trel', 'tretin', 'triptan', 'vir', 'vudine', 'zepam',
+                'zolam', 'zosin']
+    for pr in prefixes:
+        for word in words:
+            if word.startswith(pr):
+                return True
+    for suff in suffixes:
+        for word in words:
+            if word.endswith(suff):
+                return True
+    return False
+
 # process each file in directory
 def extract_entities(tokens):
     drug_bank = load_drug_bank()
     entities = []
-    for t in tokens:
-        word = t[0]
-        entity = {
-            'offset': f"{t[1]}-{t[2]}",
-            'text': word
-        }
-        if word in drug_bank:
-            entity["type"] = drug_bank[word]
-            entities.append(entity)
-        elif word.isupper():
-            entity["type"] = "brand"
-            entities.append(entity)
-        elif word[-5:] in ['azole', 'idine', 'amine', 'mycin']:
-            entity["type"] = "drug"
-            entities.append(entity)
+    for t in range(len(tokens)):
+        added = False
+        i = 0
+        while not added:
+            word = tokens[t][0]
+            word = word.lower()
+            if i > 0:
+                lower_words = " "
+                for k in range(0, i):
+                    try:
+                        lower_words = " ".join([lower_words, tokens[t+k][0]]).lower()
+                        print(lower_words)
+                    except IndexError:
+                        continue
+            else:
+                lower_words = word.lower()
+            entity = {
+                'offset': f"{tokens[t][1]}-{tokens[t][2]}",
+                'text': word
+            }
+            if lower_words in drug_bank:
+                entity["type"] = drug_bank[word]
+                entities.append(entity)
+                added = True
+            elif lower_words.isupper():
+                entity["type"] = "brand"
+                entities.append(entity)
+                added = True
+            elif check_suffixes(word):
+                entity["type"] = "drug"
+                entities.append(entity)
+                added = True
+            i = i+1
+            if i == 3:
+                added = True
     return entities
 
 
