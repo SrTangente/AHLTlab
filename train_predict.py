e6f01cc88222779598884d936e89a908a200cfd5
SrTangente <josejuangalan@gmail.com>
2021-03-11 19:15:03 +0100

    Update train_predict.py

--- train_predict.py
+++ train_predict.py
@@ -1,17 +1,37 @@
 import sys
 from feature_extractor import *
-from itertools import chain
-import nltk
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.preprocessing import LabelBinarizer
-import sklearn
+from xml.dom.minidom import parse
 import pycrfsuite
 
-train_sents = sys.argv[1]
+datadir = sys.argv[1]
 outfile = sys.argv[2]
 
-X_train = [extract_features(s) for s in train_sents]
-y_train = [get_tag(s) for s in train_sents]
+X_train = []
+y_train = []
+
+i = 0
+
+for f in listdir(datadir):
+    # parse XML file , obtaining a DOM tree
+    tree = parse(datadir + "/" + f)
+    # process each sentence in the file
+    sentences = tree.getElementsByTagName("sentence")
+    for s in sentences:
+        sid = s.attributes["id"].value # get sentence id
+        stext = s.attributes["text"].value # get sentence text
+        # load ground truth entities .
+        gold = []
+        entities = s.getElementsByTagName("entity")
+        for e in entities:
+            # for discontinuous entities , we only get the first span
+            offset = e.attributes["charOffset"].value
+            (start, end) = offset.split(";")[0].split("-")
+            gold.append((int(start), int(end), e.attributes["type"].value))
+        # tokenize text
+        tokens = tokenize(stext)
+        X_train[i] = extract_features(tokens)
+        y_train[i] = [get_tag(token, gold) for token in tokens]
+
 
 
 trainer = pycrfsuite.Trainer(verbose=False)
@@ -29,3 +49,32 @@ trainer.set_params({
 })
 
 trainer.train('trainer.crfsuite')
+
+# TODO make predictions
+
+testdir = './data/devel/'
+
+tagger = pycrfsuite.Tagger()
+tagger.open('conll2002-esp.crfsuite')
+
+for f in listdir(testdir):
+    test_tree = parse(testdir + "/" + f)
+    test_sentences = tree.getElementsByTagName("sentence")
+    test_texts = []
+
+    for s in sentences:
+        sid = s.attributes["id"].value # get sentence id
+        stext = s.attributes["text"].value # get sentence text
+        test_texts.append(stext)
+        # load ground truth entities .
+        gold = []
+        entities = s.getElementsByTagName("entity")
+        for e in entities:
+            # for discontinuous entities , we only get the first span
+            offset = e.attributes["charOffset"].value
+            (start, end) = offset.split(";")[0].split("-")
+            gold.append((int(start), int(end), e.attributes["type"].value))
+        # tokenize text
+        tokens = tokenize(stext)
+        print("Predicted:", ' '.join(tagger.tag(extract_features(tokens))))
+        print("Correct:  ", ' '.join([get_tag(token, gold) for token in tokens]))
