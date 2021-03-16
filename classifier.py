48558aee28507fbb5b8f0e2b4734734b58874d4a
JanRodriguez <janrodriguezmiret@gmail.com>
2021-03-16 13:37:07 +0100

    first workflow complete: 84% and 46% macro f1 in train/devel

--- classifier.py
+++ classifier.py
@@ -4,18 +4,47 @@ import pycrfsuite
 
 def output_entities(sid, tokens, tags):
     last_tag = "O"
+    entity_started = False
     i = 0
     while i < len(tags):
 
+        # A new entity starts
         if tags[i][0] == "B":
+
+            # Check if we have an entity already started and if so, finish it
+            if entity_started:
+                print(f"{sid}|{entity[1]}-{entity[2]}|{entity[0]}|{last_tag[2:]}")
+
+            entity_started = True
             entity = tokens[i]
-        # TODO: We have to know when an entity has "finished"
-        #  think about the following tag sequences:
-        #  B-drug -> B-drug (First entity finished)
-        #  B-drug -> O (entity finished)
-        #  I-drug -> I-drug (Entity not finished)
-        #  I-drug -> O (Entity finished)
-        #  I-drug -> B-drug (First entity finished)
+
+        # The current entity continues, update it
+        elif tags[i][0] == "I":
+
+            # In case we found an I without a B that makes sense, it is treated as a B:
+            # update tags and repeat the same iteration (this time like if it was a B)
+            if tags[i][2:] != last_tag[2:]:
+                sys.stderr.write(f"Some entity in {sid} had a different starting B: {tags}\nIt is treated as a B\n\n")
+                b_tag = 'B-' + tags[i][2:]
+                tags[i] = b_tag
+                continue
+
+            updated_entity = (entity[0] + ' ' + tokens[i][0], entity[1], tokens[i][2])
+            entity = updated_entity
+
+
+        # Finish the entity
+        elif tags[i][0] == "O" and entity_started:
+
+            print(f"{sid}|{entity[1]}-{entity[2]}|{entity[0]}|{last_tag[2:]}")
+            entity_started = False
+
+        last_tag = tags[i]
+        i += 1
+
+    # Finish the possible remaining entity (at the end of the sentence)
+    if entity_started:
+        print(f"{sid}|{entity[1]}-{entity[2]}|{entity[0]}|{last_tag[2:]}")
 
 
 if __name__ == "__main__":
